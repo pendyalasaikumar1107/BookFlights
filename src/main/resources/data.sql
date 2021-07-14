f98ae37d7306ef70fd9aac82cf085e580a7bad51
BeaulahDixcy <beaulahdixcy.ms@gmail.com>
2021-07-14 12:30:04 +0530

    Created cart table and save the flights

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,8 @@ CREATE TABLE Travellers(travellerid INT,flightid INT, numberoftravellers INT, tr
 
 INSERT INTO Travellers(travellerid,flightid,numberoftravellers,travellername)
 VALUES(10001,20001,4,'person1');
+
+
+CREATE TABLE IF NOT EXISTS  Cart ( flightid INT, cartid INT, airwaysname VARCHAR(200), source VARCHAR(200),destination VARCHAR(200),seats INT); 
+INSERT INTO Cart( flightid, cartid, airwaysname, source,destination ,seats )
+VALUES(1,2,'SAS','Chennai','Dubai',5);
